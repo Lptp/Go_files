b294e870ba57699b9d34ef0df0d68ee629f360a3
Lptp <saavento@protonmail.com>
2019-07-03 20:43:15 +0200

    Adding math pow to scope

--- variables/scope/main.go
+++ variables/scope/main.go
@@ -1,24 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 var a = "variable"
 
-func mas(x int) int {
-	return 2 ^ x
+func mas(x float64) float64 {
+	return math.Pow(2, x)
 }
 
 func main() {
-	b := 17
+	b := 17.0
 
-	mas := mas(8)
+	mas := mas(b)
 	fmt.Printf("%v \t %v \t %v \n", a, b, mas)
 	foo()
 }
 
 func foo() {
-	b := 54
+	b := 54.0
 
-	mas := mas(16)
+	mas := mas(b)
 	fmt.Printf("%v \t %v \t %v \n", a, b, mas)
 }
